6baab6587c344af91e6772b62c2a977525729374
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-04-22 17:38:25 +0300

    test5

--- src/api/fetchImage.js
+++ src/api/fetchImage.js
@@ -1,9 +1,13 @@
-import customAxios from './config';
+import axios from 'axios';
 
 const fetchImage = (query, page = 1) => {
-    return customAxios('/api', {
+    return axios.get('https://pixabay.com/api', {
         params: {
+            key: '34119717-c2cb4bf5c1e24db7e8481730d',
             q: query,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            per_page: 12,
             page
         },
     });
