c1580b5dbcb61ebbae4161f0fd64aae935a1d1f2
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-04-23 13:11:47 +0300

    fix#3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import Notiflix from 'notiflix';
 
 import fetchImage from '../api/fetchImage';
 
@@ -13,7 +14,7 @@ export class App extends Component {
     query: '',
     images: [],
     totalHits: null,
-    page: null,
+    page: 1,
     isLoading: false,
     showModal: false,
     modalImg: {
@@ -22,7 +23,36 @@ export class App extends Component {
     },
   };
 
-  componentDidUpdate = (_prevProps, prevState) => {
+  componentDidUpdate = async (_prevProps, prevState) => {
+    const { query, page } = this.state;
+    if (prevState.query !== this.state.query) {
+      try {
+        const { data } = await fetchImage(query);
+        this.setState({
+          images: data.hits,
+          totalHits: data.totalHits,
+          isLoading: false,
+        });
+        Notiflix.Notify.success(`${data.totalHits} results found for your query.`);
+      } catch (error) {
+        console.log(error);
+      };
+    };
+
+    if (prevState.page !== page) {
+      try {
+        const { data: { hits } } = await fetchImage(query, page);
+        this.setState((state) => {
+          return {
+            images: [...state.images, ...hits],
+            isLoading: false,
+          };
+        });
+      } catch (error) {
+          console.log(error);
+        }
+    };
+
     if (this.state.images.length !== prevState.images.length) {
       setTimeout(() => {
         window.scrollBy({
@@ -33,35 +63,24 @@ export class App extends Component {
     };
   };
 
-  handleOnSubmit = async (evt) => {
+  handleOnSubmit = (evt) => {
     evt.preventDefault();
     const { query } = evt.target;
-    this.setState({ isLoading: true });
-    try {
-      const { data } = await fetchImage(query.value)
-      this.setState({
-        query: query.value,
-        images: data.hits,
-        totalHits: data.totalHits,
-        page: 2,
-        isLoading: false,
-      });
-    } catch (error) {
-      console.log(error);
-    }
+    if (this.state.query !== query.value) {
+      this.setState({ query: query.value, page: 1, isLoading: true });
+      return;
+    };
+    Notiflix.Notify.info('You have already found images for this request');
   };
 
-  handleLoadMore = async () => {
-    this.setState({ isLoading: true });
-    const { data: { hits } } = await fetchImage(this.state.query, this.state.page);
-
+  handleLoadMore = () => {
     this.setState((state) => {
       return {
-        images: [...state.images, ...hits],
-        page: state.page + 1,
-        isLoading: false,
+        page: (state.page + 1),
+        isLoading: true,
       };
     });
+    
   };
 
   handleOpenModal = ({target: {id}}) => {
